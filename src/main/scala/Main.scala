2e2ce1c6bb22a95279dc61550f3fe28150e674ce
Rakesh Yadav <rakesh_yadav@epam.com>
2026-04-02 13:53:15 +0530

    adding scalafmt to the repo

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,18 @@
 import org.apache.spark.sql.SparkSession
 
 object Main {
+
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder()
+    val spark = SparkSession
+      .builder()
       .appName("ETL AI Use Cases")
+      .master("local[*]")
       .getOrCreate()
 
+    import spark.implicits._
+    Seq(1, 2, 3).toDS().show(false)
     // Your ETL logic here
-    
+
     spark.stop()
   }
 }
